3b7d78e8b1b0cf6671a0abf4808da0845144ede9
NutsaMargvelashvili <73401354+NutsaMargvelashvili@users.noreply.github.com>
2024-01-29 19:58:38 +0400

    Add files via upload

---
+++ fast-react-pizza/src/features/user/Username.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const Username = () => {
+  const { username } = useSelector((state) => state.user);
+
+  if (!username) return null;
+  return <div className={'text-sm font-semibold md:block'}>{username}</div>;
+};
+
+export default Username;
---
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './Header.jsx';
+import CartOverview from '../features/cart/CartOverview.jsx';
+import { Outlet, useNavigation } from 'react-router-dom';
+import Loader from './Loader.jsx';
+
+const AppLayout = () => {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+  return (
+    <div className={'grid h-screen grid-rows-[auto_1fr_auto]'}>
+      {isLoading && <Loader />}
+      <Header />
+      <div className={'overflow-scroll'}>
+        <main className={'mx-auto max-w-3xl overflow-scroll'}>
+          <Outlet />
+        </main>
+      </div>
+      <CartOverview />
+    </div>
+  );
+};
+
+export default AppLayout;
